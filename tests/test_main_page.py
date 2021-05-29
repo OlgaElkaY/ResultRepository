1ba6e86fe0bcfebdc704595019cf9576ebcb1f26
oyakovleva <11011981_ujl2>
2021-05-29 09:48:11 +0500

    add check element is with exception

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,5 +1,5 @@
 import time
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -10,3 +10,8 @@ def test_guest_can_go_to_login_page(browser):
     time.sleep(5)
 
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
